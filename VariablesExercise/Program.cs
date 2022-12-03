fe1b6b1ab6c2b0941570fcc71a83f211a12388fd
Jose Zepeda <josezepedavaldez30@gmail.com>
2022-12-03 11:49:46 -0800

    initial commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace VariablesExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿// Declaring variables and initialize
+string name = "Jose";
+int age = 25;
+char middleInitial = 'D';
+bool isOver18 = true;
+double currentTemp = 77.9;
+decimal currentPrice = 29.99m;
+
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Middle Initial: {middleInitial}");
+Console.WriteLine($"Is Over 18: {isOver18}");
+Console.WriteLine($"current Temp: {currentTemp}");
+Console.WriteLine($"current Price: {currentPrice}");
